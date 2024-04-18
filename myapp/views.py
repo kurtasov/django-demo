cf76615a9d1a575da675ce3a75e69fcec8fc11c0
kurtasov <kurtasov@users.noreply.github.com>
2024-04-18 16:35:42 +0300

    Slug processing

--- myapp/views.py
+++ myapp/views.py
@@ -9,3 +9,12 @@ def hello(request):
         'footer_text': 'До свидания!'
     }
     return HttpResponse(template.render(context, request))
+
+
+def hello_name(request, slug):
+    template = loader.get_template('hello_world.html')
+    context = {
+        'data': ['test1', 'test2', 'test3'],
+        'footer_text': f'Привет, {slug}!'
+    }
+    return HttpResponse(template.render(context, request))
--- myservice/urls.py
+++ myservice/urls.py
@@ -21,4 +21,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
+    path('hello/<slug:slug>', views.hello_name),
 ]
