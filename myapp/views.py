14f541821c08e92e7e7edc4877d8a2f4cc7d586e
kurtasov <kurtasov@users.noreply.github.com>
2024-04-18 15:47:19 +0300

    "Hello world" template implemented

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+def hello(request):
+    template = loader.get_template('hello_world.html')
+    context = {
+        'data': ['test1', 'test2', 'test3'],
+        'footer_text': 'До свидания!'
+    }
+    return HttpResponse(template.render(context, request))
